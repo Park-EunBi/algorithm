5867d542d5ff3dca6ec12909aacb60f3468fd1a3
Park-EunBi <parkeb555@gmail.com>
2022-02-27 14:37:53 +0900

    2022/02/27

---
+++ programmers/2022/level1_내적.py
@@ -0,0 +1,14 @@
+def solution(a, b):
+    answer = 0
+    for i, j in zip(a, b):
+        answer += (i * j)
+    return answer
+
+
+
+print(solution([1,2,3,4], [-3,-1,0,2]))
+print(solution([-1,0,1], [1,0,-1]))
+
+'''
+    return sum([x*y for x, y in zip(a,b)])
+'''
