836655a3f8063e79ce1897a1629388eea893c092
Park-EunBi <parkeb555@gmail.com>
2022-03-22 23:24:46 +0900

    2022/03/22

---
+++ python/list.py
@@ -0,0 +1,19 @@
+# create list
+a = [] # a = list()
+
+# add data
+a = [0]
+a. append(True)
+a.insert(2, 'hi')
+print(a)
+
+# list slicing
+print((a[0:2]))
+print(a[:-2])
+print(a[::2])
+
+# remove data
+del a[2]
+print(a)
+a.remove(True)
+print(a)
