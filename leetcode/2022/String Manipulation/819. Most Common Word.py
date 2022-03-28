21716f307efc94f89ec59bdb9af39f63a5e00fa2
Park-EunBi <parkeb555@gmail.com>
2022-03-28 23:00:23 +0900

    2022/03/28 수정중

---
+++ leetcode/2022/String Manipulation/819. Most Common Word.py
@@ -0,0 +1,35 @@
+import re, collections
+from typing import List
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        # lower and remove . ,
+        paragraph = re.sub(r"[^a-z0-9]"," ",paragraph.lower())
+
+        # split
+        word = paragraph.split(sep=' ')
+
+        # remove banned from word
+        for w in word:
+            if w in banned:
+                word.remove(w)
+
+        # count the frequency of a word
+        cnt = collections.Counter(word)
+
+        return cnt.most_common(1)[0][0]
+
+'''
+(결과)
+Wrong Answer
+
+Input     "Bob. hIt, baLl"
+          ["bob", "hit"]
+Output    ""
+Expected  "ball" 
+
+'''
+
+
+s = Solution()
+print(s.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.","hit"))
+print(s.mostCommonWord("a.", ""))
