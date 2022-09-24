39890b51d758c5aee80eeb043ef4b327ece69087
Park-EunBi <parkeb555@gmail.com>
2022-09-24 21:31:10 +0900

    FIX: 파일 오류 수정

---
+++ leetcode/2022/Linear Data Structure/[array] 121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_price = 0
+
+        for i, price in enumerate(prices):
+            for j in range(i, len(prices)):
+                max_price = max(prices[j] - price, max_price)
+
+        return max_price
+
+s = Solution()
+print(s.maxProfit([7,1,5,3,6,4])) # 5
+print(s.maxProfit([7,6,4,3,1])) # 0
+print(s.maxProfit([2,4,1])) # 2
+print(s.maxProfit([3,2,6,5,0,3])) # 4
