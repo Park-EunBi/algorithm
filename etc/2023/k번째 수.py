00711c7735d78d717aa6bd2c56484495c46693a0
Park-EunBi <parkeb555@gmail.com>
2023-01-08 21:21:26 +0900

    2023/01/08

---
+++ etc/2023/k번째 수.py
@@ -0,0 +1,12 @@
+import sys
+# sys.stdin = open("input.txt", "rt")
+
+t = int(input())
+for i in range(t):
+    n, s, e, k = map(int, input().split())
+    nums = list(map(int, input().split()))
+
+    nums = nums[s-1: e]
+    nums.sort()
+    print("#%d %d" %(i + 1, nums[k-1]))
+
