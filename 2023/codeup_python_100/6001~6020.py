d23f59b8442ec2a25b1238229cd38bed82dd50cd
Park-EunBi <parkeb555@gmail.com>
2023-04-16 19:59:18 +0900

    2023/04/16

---
+++ 2023/codeup_python_100/6001~6020.py
@@ -0,0 +1,76 @@
+# 6001
+print("Hello")
+
+# 6002
+print("Hello World")
+
+# 6003
+print("Hello\nWorld")
+
+# 6004
+print('\'Hello\'')
+
+# 6005
+print('"Hello World"')
+
+# 6006
+print("\"!@#$%^&*()\'")
+
+# 6007
+print("\"C:\Download\\'hello\'.py\"")
+
+# 6008
+print("print(\"Hello\\nWorld\")")
+
+# 6009
+c = input()
+print(c)
+
+# 6010
+i = int(input())
+print(i)
+
+# 6011
+f = float(input())
+print(f)
+
+# 6012
+a = int(input())
+b = int(input())
+print(a)
+print(b)
+
+# 6013
+a = input()
+b = input()
+print(b)
+print(a)
+
+# 6014
+f = float(input())
+print(f*3)
+
+# 6015
+a, b = map(int, input().split())
+print(a)
+print(b)
+
+# 6016
+x, y = input().split()
+print(y + ' ' + x)
+
+# 6017
+x = input()
+print((x + ' ') * 3)
+
+# 6018
+h, m = input().split(':')
+print(h+':'+m)
+
+# 6019
+y, m, d = input().split('.')
+print(d+'-'+m+'-'+y)
+
+# 6020
+f,b = input().split('-')
+print(f+b)
---
+++ 2023/codeup_python_100/6021~6040.py
@@ -0,0 +1,81 @@
+# 6021
+s = input()
+for i in s:
+    print(i)
+
+# 6022
+date = input()
+print(date[:2], date[2:4], date[4:])
+
+# 6023
+h, m, s = input().split(':')
+print(m)
+
+# 6024
+s1, s2 = input().split()
+print(s1+s2)
+
+# 6025
+a, b = map(int, input().split())
+print(a+b)
+
+# 6026
+a = float(input())
+b = float(input())
+print(a+b)
+
+# 6027
+n = int(input())
+print('%x' %n)
+
+# 6028
+n = int(input())
+print('%X'%n)
+
+# 6029
+n = int(input(), 16)
+print('%o' %n)
+
+# 6030
+s = input()
+print(ord(s))
+
+# 6031
+n = int(input())
+print(chr(n))
+
+# 6032
+n = int(input())
+print(-n)
+
+# 6033
+s = input()
+print(chr(ord(s) + 1))
+
+# 6034
+a, b = map(int, input().split())
+print(a - b)
+
+# 6035
+f1, f2 = map(float, input().split())
+print(f1 * f2)
+
+# 6036
+s, n = input().split()
+print(s*int(n))
+
+# 6037
+n = int(input())
+s = input()
+print(s * n)
+
+# 6038
+a, b = map(int, input().split())
+print(a**b)
+
+# 6039
+f1, f2 = map(float, input().split())
+print(f1**f2)
+
+# 6040
+
