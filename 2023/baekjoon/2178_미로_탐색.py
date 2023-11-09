6a209db721f6f47bab03bcde8fd96b2354f160e9
Park-EunBi <parkeb555@gmail.com>
2023-11-09 22:47:43 +0900

    2023/11/09

--- 2023/baekjoon/2178_미로_탐색.py
+++ 2023/baekjoon/2178_미로_탐색.py
@@ -1,47 +1,41 @@
-# (1, 1) to (n, m) 까지의 최소 칸의 수
-# -> (n, m) to (1, 1)
 from collections import deque
 n, m = map(int, input().split())
-# 1: 이동 가능, 0: 이동 불가
+
 board = [
     list(map(int, input()))
     for _ in range(n)
 ]
 
-ret = 0
-visited = [
-    [0] * (m)
-    for _ in range(n)
-]
 
-nx = [0, 1, 0, -1]
-ny = [1, 0, -1, 0]
 
-# dfs
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+# 최소 칸의 개수 -> bfs
 def bfs(x, y):
-    # 1. 선언
+    # 생성, 초기화
     q = deque()
-    # 2. 초기화
     q.append((x, y))
-    visited[x][y] = 0
-    # 3. 반복
+
+    # 반복
     while q:
         x, y = q.popleft()
-        # 사방 확인
+
+        # 이동
         for i in range(4):
-            dx = x + nx[i]
-            dy = y + ny[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            # 범위 확인
-            if 0 <= dx < n and 0 <= dy < m and visited[dx][dy] == 0:
-                # 범위 내에 있고, 방문하지 않았지만, 갈 수 있다면
-                if board[dx][dy] == 1:
-                    visited[dx][dy] = visited[x][y] + 1
-                    q.append((dx, dy))
-                elif board[dx][dy] == 0:
-                    visited[dx][dy] = 0
+            # 범위 조건
+            if nx < 0 or nx >= n or ny < 0 or ny >= m or board[nx][ny] == 0:
+                continue
 
+            # 이동
+            if board[nx][ny] == 1:
+                board[nx][ny] = board[x][y] + 1
+                q.append((nx, ny))
+
+    return board[n-1][m-1]
 
 # main
-bfs(n-1, m-1)
-print(visited[0][0] + 1)
+print(bfs(0, 0))
