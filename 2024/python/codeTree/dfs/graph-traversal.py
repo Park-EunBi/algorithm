0995be8e93456ee9f5858503364089a18915a032
Park-EunBi <parkeb555@gmail.com>
2024-02-13 21:13:41 +0900

    2024/02/13

---
+++ 2024/python/codeTree/dfs/graph-traversal.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+cnt = 0
+def dfs(node):
+    global cnt
+    # visited[node] = 1 # 첫 노드를 밖에서 방문처리하면 for 문 안에서 처리가능
+    for n in graph[node]:
+        if not visited[n]:
+            visited[n] = 1
+            cnt += 1
+            dfs(n)
+
+visited[1] = 1 # 첫 노드를 밖에서 방문처리하기
+dfs(1)
+print(cnt)
---
+++ 2024/python/codeTree/dfs/seperate-village.py
@@ -0,0 +1,60 @@
+# 마을의 개수, 마을에 있는 사람 수 오름차순 출력
+# 1: 사람, 0: 벽
+
+n = int(input())
+board = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+country = []
+cnt = 0
+dxs, dys = [0, 0, 1, -1], [1, -1, 0, 0]
+
+def canGo(x, y):
+    # 1. 격자 외부
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False
+    # 2. 방문 여부, 장애물 확인
+    if board[x][y] == 0:
+        return False
+    return True
+
+def dfs(x, y):
+    global cnt
+
+    # 방문처리
+    board[x][y] = 0
+    # 인접 노드 방문
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if canGo(nx, ny):
+            cnt += 1
+            dfs(nx, ny)
+
+for i in range(n):
+    for j in range(n):
+        if canGo(i, j): # 방문 가능할 경우
+            board[i][j] = 0 # 방문 처리
+            cnt = 1 # 사람 수 초기화
+            dfs(i, j) # 탐색
+            country.append(cnt) # 주민 수 추가
+
+
+print(len(country))
+for c in sorted(country):
+    print(c)
+
+'''
+5
+1 0 1 0 1
+1 1 1 0 1
+0 1 0 1 1
+1 1 1 0 0
+1 1 0 1 1
+
+3
+2
+4
+11
+'''
