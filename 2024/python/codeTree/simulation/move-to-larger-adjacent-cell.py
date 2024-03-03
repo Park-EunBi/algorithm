e9f4dc61343ceffb0700baca59ad6dc6e91c803f
Park-EunBi <parkeb555@gmail.com>
2024-03-03 22:35:06 +0900

    2024/03/03

--- 2024/python/codeTree/simulation/move-to-larger-adjacent-cell.py
+++ 2024/python/codeTree/simulation/move-to-larger-adjacent-cell.py
@@ -3,18 +3,15 @@ board = [list(map(int, input().split())) for _ in range(n)]
 
 # 상하좌우
 dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-move = True
-
+move = True # 초깃값 주의
 
 def in_range(x, y):
     if x < 0 or x >= n or y < 0 or y >= n:
         return False
     return True
 
-
 def simulation(x, y):
     global move
-    # move = False
     # 1. 현재 값 확인
     now = board[x][y]
     # 2. 이동
@@ -22,8 +19,7 @@ def simulation(x, y):
         nx = x + dx
         ny = y + dy
         if in_range(nx, ny) and now < board[nx][ny]:
-            move = True
-            return nx, ny
+            return nx, ny # 모든 영역 탐색하지 않도록 조건 만족하면 바로 return
     move = False
     return x, y
 
@@ -31,6 +27,6 @@ def simulation(x, y):
 x = r - 1
 y = c - 1
 
-while move:
-    print(board[x][y])
+while move: # 반복 조건 주의
+    print(board[x][y], end=' ')
     x, y = simulation(x, y)
