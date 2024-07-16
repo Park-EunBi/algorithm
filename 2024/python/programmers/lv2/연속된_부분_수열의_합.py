54a5926354e9912dfb39bc0fa2e4b9b631ee547f
Eunbi Park <87464975+Park-EunBi@users.noreply.github.com>
2024-07-16 15:00:01 +0900

    2024/07/16

--- 2024/python/programmers/lv2/연속된_부분_수열의_합.py
+++ 2024/python/programmers/lv2/연속된_부분_수열의_합.py
@@ -1,38 +1,57 @@
 def solution(sequence, k):
-        n = len(sequence)
-        start, end, sum = 0, 0, sequence[0]
-        ans = [0, float('inf')]
+    n = len(sequence)
+    end = 0
+    total = 0
+    ans = []
+    
+    for start in range(n):
+        while total < k and end < n:
+            total += sequence[end]
+            end += 1
+            
+        if total == k:
+            ans.append((start, end -1, end - 1 - start)) # start, end, 길이
         
-        while start <= end and end < n:
-            # 1. 현재 수열의 합 < k
-            # end += 1
-            if sum < k and end < n-1: # end += 1 할 것이기에 범위 체크
-                end += 1
-                sum += sequence[end]
+        total -= sequence[start]
+    
+    ans.sort(key = lambda x: x[2])
+    return ans[0][:2]
+
+# def solution(sequence, k):
+#         n = len(sequence)
+#         start, end, sum = 0, 0, sequence[0]
+#         ans = [0, float('inf')]
+        
+#         while start <= end and end < n:
+#             # 1. 현재 수열의 합 < k
+#             # end += 1
+#             if sum < k and end < n-1: # end += 1 할 것이기에 범위 체크
+#                 end += 1
+#                 sum += sequence[end]
             
-            # 2. 현재 수열의 합 > k
-            # start += 1
-            elif sum > k:
-                sum -= sequence[start]
-                start += 1
+#             # 2. 현재 수열의 합 > k
+#             # start += 1
+#             elif sum > k:
+#                 sum -= sequence[start]
+#                 start += 1
                 
-            # 3. 현재 수열의 합 == k
-            elif sum == k:
-                # 기존에 등록된 수열의 길이와 비교 
-                # 같으면 인덱스 작은 것을 등록해야 하기에 갱신하지 않는다
-                if ans[1] - ans[0] > end - start: 
-                    ans = [start, end]
+#             # 3. 현재 수열의 합 == k
+#             elif sum == k:
+#                 # 기존에 등록된 수열의 길이와 비교 
+#                 # 같으면 인덱스 작은 것을 등록해야 하기에 갱신하지 않는다
+#                 if ans[1] - ans[0] > end - start: 
+#                     ans = [start, end]
                 
-                # sum = k를 만족하는 더 짧은 수열이 있을 수 있기에 start += 1 
-                sum -= sequence[start]
-                start += 1
+#                 # sum = k를 만족하는 더 짧은 수열이 있을 수 있기에 start += 1 
+#                 sum -= sequence[start]
+#                 start += 1
             
-            # 4. (1, 2, 3) 조건을 만족하지 못했는데 end를 갱신하지 못해 무한루프인 경우 방지
-            else:
-                if end == n-1:
-                    break
+#             # 4. (1, 2, 3) 조건을 만족하지 못했는데 end를 갱신하지 못해 무한루프인 경우 방지
+#             else:
+#                 if end == n-1:
+#                     break
                 
-        return ans
+#         return ans
 
 # def solution(sequence, k):
 #     n = len(sequence)
